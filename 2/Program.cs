263931fec927ee6588f634d9ece00e53d324e011
Ilya <ilya_rogachev_89@mail.ru>
2022-04-02 22:13:28 +0400

    Task_2 ready

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Показать числа Фибоначчи
+Console.WriteLine("До какого числа считать последовательность Фибоначчи");
+int n = Convert.ToInt32(Console.ReadLine());
+int first = 1;
+int second = 1;
+Console.Write(first + " " + second + " ");
+  while (n >= first + second)
+  {
+      int sum =  first + second;
+      Console.Write(sum + " ");
+        first = second;
+        second = sum;
+  }
