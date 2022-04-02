281db44a35c130917893b438232811f80b58bf73
Ilya <ilya_rogachev_89@mail.ru>
2022-04-02 22:56:08 +0400

    task_2_2 ready

---
+++ 2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать числа Фибоначчиee
+Console.WriteLine("Сколько чисел Фибонначи вывести в консоль?");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int sum = 0;
+int first = 1;
+int second = 1;
+while (i <= number)
+{
+    if(i == 1 || i == 2) 
+    {
+        Console.Write("1 ");
+        i++;
+    }
+    else
+    {
+        sum = first + second;
+        Console.Write(sum + " ");
+        first = second;
+        second = sum;
+        i++;
+    }
+}
